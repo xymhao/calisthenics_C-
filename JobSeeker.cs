b514f6551285bca83164607fcad4ed385703e863
许玥明 <xuyueming@beisen.com>
2020-03-13 10:53:37 +0800

    新增Job Applications（求职申请）对象

---
+++ JobSeeker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calisthenics
+{
+    public class JobSeeker
+    {
+        public JobSeeker(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is JobSeeker jobSeeker && jobSeeker.Equals(this);
+        }
+
+        public bool Equals(JobSeeker obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return obj.Name.Equals(Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
---
+++ Resume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calisthenics
+{
+    public class Resume
+    {
+        public JobSeeker Seeker { get; set; }
+
+        public string Infos { get; set; }
+    }
+}
