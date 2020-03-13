716ff3c7578d4198406060467d3a75e68cd0c505
许玥明 <xuyueming@beisen.com>
2020-03-13 10:43:04 +0800

    新增employee对象，重写equal方法

---
+++ Employer.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calisthenics
+{
+    public class Employer
+    {
+        public Employer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Employer employer && employer.Equals(this);
+        }
+
+        public bool Equals(Employer obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return obj.Name.Equals(Name);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
