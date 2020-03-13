32f8a95d71bbb0a82af5686c9098ab66b7f6e409
许玥明 <xuyueming@beisen.com>
2020-03-13 10:33:19 +0800

    创建job对象

---
+++ Job.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calisthenics
+{
+    public class Job
+    {
+        public string JobType { get; set; }
+
+        public string JobName { get; set; }
+
+    }
+}
